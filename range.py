b7ed58ec39c863f10145ccd2ec39facc989c7376
Anubhav Singh <66737188+4nu6h4v51ngh@users.noreply.github.com>
2020-09-15 20:21:19 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,15 @@
+for i in range(10):
+	print(i,end=' ')
+print('\n')
+for i in range(2,10):
+	print(i,end=' ')
+print('\n')
+for i in range(0,21,2):
+	print(i,end=(' '))
+print('\n')
+r=range(0,9)
+for i in r:
+	print(i,end=' ')
+print('\n')
+print(r[1:3])
+print(type(r))
